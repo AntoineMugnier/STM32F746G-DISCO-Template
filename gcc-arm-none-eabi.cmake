001c47b4033609fc4e1b1a71af14b0f89d4c73d4
Antoine Mugnier <amugnier@europe.com>
2024-05-11 19:09:30 +0200

    toolchain file updated
    - Linker warnings solved
    - Increased warning restrictivity
    - file reorganized

--- gcc-arm-none-eabi.cmake
+++ gcc-arm-none-eabi.cmake
@@ -5,21 +5,33 @@ set(CMAKE_SYSTEM_PROCESSOR          arm)
 
 # Some default GCC settings
 # arm-none-eabi- must be part of path environment
-set(DEPENDENCY_INFO_OPTIONS "-MMD   -MP    -MF \"$(@:%.o=%.d)\"") #unused
+set(DEPENDENCY_INFO_OPTIONS "-MMD   -MP    -MF \"$(@:%.o=%.d)\"") 
 set(TOOLCHAIN_PREFIX                arm-none-eabi-)
 set(MCU_FLAGS "-mcpu=cortex-m7 -mthumb -mfpu=fpv5-sp-d16 -mfloat-abi=hard" )
-set(OPT "-Og")
-set(CMAKE_C_FLAGS                         "${MCU_FLAGS} ${OPT} ${DEPENDENCY_INFO_OPTIONS} -DSTM32F746xx -Wall -std=gnu99 -fdata-sections -ffunction-sections")
-set(CMAKE_ASM_FLAGS                       "${MCU_FLAGS} ${OPT}  -Wall -fdata-sections -ffunction-sections")
-set(LINKER_SCRIPT                 "${PROJECT_SOURCE_DIR}/STM32F746NGHx_FLASH.ld")
-set(CMAKE_EXE_LINKER_FLAGS "${MCU_FLAGS} -specs=nano.specs -T${LINKER_SCRIPT} -lc -lm -lnosys -Wl,-Map=${PROJECT_NAME}.map,--cref -Wl,--gc-sections ")
+set(WARNING_FLAGS "-Wall -Wextra -Wpedantic")
 
+# Parameters varying depending on the build type
 if(CMAKE_BUILD_TYPE MATCHES Debug)
     set(OPT "-O0 -g3")
 endif()
 if(CMAKE_BUILD_TYPE MATCHES Release)
     set(OPT " -Os -g0")
 endif()
+
+# Compiler flags
+set(CMAKE_C_FLAGS   "${MCU_FLAGS} ${OPT} ${DEPENDENCY_INFO_OPTIONS} ${WARNING_FLAGS}  -fdata-sections -ffunction-sections -DSTM32F746xx")
+
+# Assembly flags
+set(CMAKE_ASM_FLAGS  "${CMAKE_C_FLAGS}")
+
+# Linker flags
+set(CMAKE_C_LINK_FLAGS "${MCU_FLAGS}")
+set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} -T \"${CMAKE_SOURCE_DIR}/STM32F746NGHx_FLASH.ld\"")
+set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} --specs=nano.specs")
+set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} -Wl,-Map=${CMAKE_PROJECT_NAME}.map -Wl,--gc-sections")
+set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} -Wl,--start-group -lc -lm -Wl,--end-group")
+set(CMAKE_C_LINK_FLAGS "${CMAKE_C_LINK_FLAGS} -Wl,--print-memory-usage")
+
 # Define which tools we use
 set(CMAKE_C_COMPILER                ${TOOLCHAIN_PREFIX}gcc)
 set(CMAKE_ASM_COMPILER              ${CMAKE_C_COMPILER})
